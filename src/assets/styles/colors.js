429efeacc6f8cf1506e8b453721fa190b4730f5d
Themba <tfmzworld@gmail.com>
2020-03-14 18:44:57 +0200

    m-001: Add gradient colour

--- src/assets/styles/colors.js
+++ src/assets/styles/colors.js
@@ -1,5 +1,6 @@
 const colors = {
   backgroundColor: '#282c34',
+  linearBackground: 'linear-gradient(135deg, rgb(96, 108, 136) 0%, rgb(63, 76, 107) 100%)',
   shadow: 'rgb(0, 0, 0) 0px 5px 20px -10px',
   whiteText: 'white',
 };
