9b07d014cd1784b21fc9647ff654868cff5e8a46
Themba <tfmzworld@gmail.com>
2020-03-14 18:45:14 +0200

    m-001: Add 404 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Experience from '../pages/Experience';
+import F0F from '../pages/404';
 
 const AppRoutes = () => (
   <div>
@@ -11,6 +12,7 @@ const AppRoutes = () => (
     <Route exact path="/about" component={About} />
     <Route exact path="/contact" component={Contact} />
     <Route exact path="/experience" component={Experience} />
+    <Route exact path="/404" component={F0F} />
   </div>
 );
 
