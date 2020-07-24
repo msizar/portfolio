fea5fcebf8dc92b589521634f38534fb559ca0e1
Themba <tfmzworld@gmail.com>
2020-07-24 20:04:36 +0200

    REMOVE ALL JS FILES

--- src/assets/styles/colors.js
+++
@@ -1,7 +0,0 @@
-const colors = {
-  backgroundColor: '#282c34',
-  linearBackground: 'linear-gradient(135deg, rgb(96, 108, 136) 0%, rgb(63, 76, 107) 100%)',
-  shadow: 'rgb(0, 0, 0) 0px 5px 20px -10px',
-  whiteText: 'white',
-};
-export default colors;
--- src/constants/links.ts
+++
@@ -1,5 +0,0 @@
-export const HOME = 'home';
-export const PROFILE = 'profile';
-export const SKILLS = 'skills';
-export const PROJECTS = 'projects';
-export const LINK_ARRAY = [HOME, PROFILE, SKILLS, PROJECTS];
